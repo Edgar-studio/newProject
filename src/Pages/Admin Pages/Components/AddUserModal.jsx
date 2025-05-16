a3864e9174996d4916a3dfd36d7921d041f63ee1
Edgar <edgaravakimyan7@gmail.com>
2025-05-16 16:30:46 +0200

    .

--- src/Pages/Admin Pages/Components/AddUserModal.jsx
+++ src/Pages/Admin Pages/Components/AddUserModal.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import Input from "../../../Components/UI/Input.jsx";
+import {useForm} from "react-hook-form";
+import useAuth from "../../../Utils/useAuth.jsx";
 
 const AddUserModal = () => {
+
+    const {handleRegister} = useAuth()
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid }
+    } = useForm({ mode: 'all' });
     return (
-        <form className='w-full h-full bg-yellow-200 flex flex-col gap-4 p-6'>
+        <form
+            onSubmit={handleSubmit(handleRegister)}
+            className='w-full h-full bg-yellow-200 flex flex-col gap-4 p-6'>
             <h2 className='text-xl font-bold'>Add New User</h2>
 
             <Input
+                register={register}
                 type='text'
                 placeholder='Enter username'
                 label='Username'
@@ -15,6 +27,7 @@ const AddUserModal = () => {
             />
 
             <Input
+                register={register}
                 type='email'
                 placeholder='Enter email'
                 label='Email'
@@ -23,6 +36,8 @@ const AddUserModal = () => {
             />
 
             <Input
+                register={register}
+
                 type='password'
                 placeholder='Enter password'
                 label='Password'
