ddbd9f9296dd36f11c102f40c6d7328bfd6d4ff3
Edgar-studio <edgaravakimyan7@gmail.com>
2025-06-09 17:04:00 +0200

    added styles to TutorInfo.jsx

--- src/Pages/Private Pages/Karate/TutorInfo.jsx
+++ src/Pages/Private Pages/Karate/TutorInfo.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 import useTutorials from "../../../Utils/useTutorials.jsx";
 
-
 const TutorInfo = () => {
-    const [tutorial, setTutorial] = useState([]);
-    const params = useParams()
+    const [tutorial, setTutorial] = useState(null);
+    const params = useParams();
 
-    const {fetchTutorials} = useTutorials()
+    const { fetchTutorials } = useTutorials();
+
     useEffect(() => {
         const loadTutorials = async () => {
             const data = await fetchTutorials();
@@ -15,26 +15,32 @@ const TutorInfo = () => {
             setTutorial(tutorialInfo);
         };
         loadTutorials();
-
-
     }, []);
 
+    if (!tutorial) return <div className="text-center text-white mt-20 text-xl">Loading...</div>;
+
     return (
+        <div className="w-full min-h-screen dark:bg-gray-900 flex flex-col items-center justify-start p-6 text-white">
+            <h1 className="text-4xl font-bold mb-6 text-center">{tutorial.title}</h1>
 
-        //stylery kargavorel
-        <div
-            className='w-full h-screen dark:bg-gray-500 flex flex-col items-center
-        justify-center p-6 dark:bg-text-white'
-        >
-            <h1 className='text-4xl'>{tutorial.title}</h1>
-            <iframe width="914" height="514" src={tutorial.video}
-
-                    allow="accelerometer; pause; autoplay; clipboard-write; encrypted-media;
-                    gyroscope; picture-in-picture; web-share"
+            <div className="w-full max-w-4xl aspect-video mb-6">
+                <iframe
+                    className="w-full h-full rounded-xl shadow-lg"
+                    src={tutorial.video}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen></iframe>
-            <p className="text-2xl">{tutorial.description}</p>
-            <p className="text-2xl ">{tutorial.body}</p>
+                    allowFullScreen
+                ></iframe>
+            </div>
+
+            <div className="w-full max-w-4xl space-y-6 px-4">
+                <p className="text-xl text-black font-semibold">
+                    {tutorial.description}
+                </p>
+                <p className="text-lg text-black leading-relaxed whitespace-pre-line">
+                    {tutorial.body}
+                </p>
+            </div>
         </div>
     );
 };
